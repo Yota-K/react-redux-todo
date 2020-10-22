639661bdf78219793dc8dd0d57ead3e8e64d9773
紅梅陽太 <powdersugar828828@gmail.com>
2020-10-22 23:16:40 +0900

    bootstrap入れた。

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import { store } from './store/index';
 import App from './App';
 
